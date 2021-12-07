94a9f0c8cb4fdcc4da99b407df365f661f84313c
Ivaylo Gerchev <38769524+codeknack@users.noreply.github.com>
2021-12-07 15:22:05 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
   <div>
     <article class="m-4 md:w-1/2 lg:w-1/3" v-for="post in posts" :key="post.id">
       <NuxtLink :to="`/post-${post.id}`">
-        <h1 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h1>
+        <h2 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h2>
       </NuxtLink>
       <p>{{ post.body }}...</p>
     </article>
