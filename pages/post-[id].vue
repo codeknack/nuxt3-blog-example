e24f0b4fc593feb7ef06b22e62152707af81afcc
Ivaylo Gerchev <38769524+codeknack@users.noreply.github.com>
2021-12-07 15:23:10 +0200

    Update post-[id].vue

--- pages/post-[id].vue
+++ pages/post-[id].vue
@@ -16,7 +16,7 @@
     </NuxtLink>
     <quote />
     <article class="m-4 md:w-1/2 lg:w-1/3">
-      <h1 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h1>
+      <h2 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h2>
       <p>{{ post.body }}</p>
     </article>
   </div>
